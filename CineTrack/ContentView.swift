790caafe7ce42c78da5ccdd3a6fe6b9b9dacfb37
Ryan Meneses <ryanmeneses.dev@gmail.com>
2025-09-29 11:43:31 -0300

    add: general project structure added

---
+++ CineTrack/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
